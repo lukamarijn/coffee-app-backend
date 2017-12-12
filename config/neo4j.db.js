7653c1ef95b487275e58ae8956db1ded3081a5a6
lukamarijn <lukabrinkmannn@gmail.com>
2017-12-12 23:15:10 +0100

    Added new neo4j driver

--- config/neo4j.db.js
+++ config/neo4j.db.js
@@ -1,5 +1,5 @@
 const neo4j = require('neo4j-driver').v1;
 
-const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "password"));
-
+/*const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "password"));*/
+const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("username1", "b.EvPgFf2SvQxe.Is4r5Zq1HFDpGGMx"));
 module.exports = driver;
