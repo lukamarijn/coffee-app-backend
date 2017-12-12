27a489ff7520ebe41b0a19d375c57f8a2ff72f09
lukamarijn <lukabrinkmannn@gmail.com>
2017-12-13 00:27:06 +0100

    testje

--- config/env/env.js
+++ config/env/env.js
@@ -12,9 +12,11 @@ if (process.env.NODE_ENV = 'test') {
     dburl = 'mongodb://localhost/coffee-app-backend-test'
 }
 
-    var dburl = process.env.NODE_ENV === 'production' ?
+   /* var dburl = process.env.NODE_ENV === 'production' ?
         'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
-        'mongodb://localhost/' + env.dbDatabase;
+        'mongodb://localhost/' + env.dbDatabase;*/
+
+   var dburl = 'mongodb://username1:password1@ds135956.mlab.com:35956/coffee-app-backend';
 
 
 module.exports = {
